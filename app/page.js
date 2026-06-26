296425afc625b24d93649f30e345317b3f2742bb
AmirAli <amiralisalehik@gmail.com>
2026-06-26 12:30:58 +0330

    change Home function to be dynamic

--- app/page.js
+++ app/page.js
@@ -1,13 +1,12 @@
-import Header from "@/components/Header";
+export const dynamic = "force-dynamic";
 import LatestProducts from "@/components/LatestProducts";
-
 
 export default function Home() {
   return (
-  <>
-  <h1>home</h1>
-  <h1 className="text-lg font-bold text-center ">محصولات پرفروش</h1>
-  <LatestProducts></LatestProducts>
-  </>
+    <>
+      <h1>home</h1>
+      <h1 className="text-lg font-bold text-center ">محصولات پرفروش</h1>
+      <LatestProducts></LatestProducts>
+    </>
   );
 }
